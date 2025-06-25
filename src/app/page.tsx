56d55491360772ebd3b15a4ae552f0c05f23c54b
ayucahyani96 <ayucahyani96@gmail.com>
2025-06-25 11:28:19 +0700

    first init

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,102 +1,170 @@
+import { Button } from "@/components/ui/button";
+import { ImageIcon, User } from "lucide-react";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    <div>
+      <nav className="h-[80px] bg-white px-10 flex items-center justify-between">
+        <div aria-label="logo" className="w-20">
+          <h1 className="font-black text-4xl">TEUAS</h1>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
+        <div aria-label="nav-container" className="flex gap-4 text-gray-400">
+          <span className="text-orange-600 opacity-100">Home</span>
+          <span>Database Alumnni</span>
+          <span>Prestasi Mahasiswa</span>
+          <span>Loker</span>
+          <span>E-Learning</span>
+        </div>
+        <div aria-label="login" className="w-20">
+          <button className="bg-orange-600 px-6 py-2 rounded text-white font-medium">
+            Login
+          </button>
+        </div>
+      </nav>
+
+      <section aria-label="hero-section" className="bg-black px-10">
+        <div className="h-[800px] grid grid-cols-2 text-white place-items-center">
+          <div className="space-y-5 max-w-[650px]">
+            <span className="text-gray-400 font-bold text-xl">TEUAS UPI</span>
+            <h1 className="text-6xl font-bold tracking-wide leading-[72px]">
+              Membangun Koneksi, Berbagi Ilmu, Meraih Masa Depan
+            </h1>
+            <p className="text-gray-400">
+              TEUAS hadir untuk mempererat jaringan alumni, mendukung mahasiswa,
+              dan membuka peluang karir bagi lulusan Teknik Elektro UPI
+            </p>
+            <Button size="lg" className="bg-orange-600">
+              Gabung Sekarang
+            </Button>
+          </div>
+          <div>
+            <div className="bg-gray-500 h-[500px] w-[500px] rounded-2xl flex items-center justify-center">
+              <User className="h-32 w-32" />
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section aria-label="about-section" className="px-10">
+        <div className="h-[800px] grid grid-cols-2 place-items-center">
+          <div className="space-y-5 max-w-[750px]">
+            <span className="text-orange-400 font-bold text-xl">IKA TEUAS</span>
+            <h1 className="text-6xl font-bold tracking-wide leading-[72px]">
+              TEKNIK ELEKTRO UNITY AND SOLIDARITY
+            </h1>
+            <p className="text-gray-400">
+              IKA TEUAS adalah komunitas alumni Teknik Elektro UPI yang bertahan
+              membangun jaringan professional, berbagi pengalaman, serta membuka
+              peluang bagi mahasiswa dan alumni dalam dunia kerja.
+            </p>
+            <Button size="lg" className="bg-orange-600">
+              Gabung Sekarang
+            </Button>
+          </div>
+          <div>
+            <div className="bg-gray-500 h-[500px] w-[500px] rounded-2xl flex items-center justify-center">
+              <ImageIcon className="h-32 w-32 text-white" />
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section aria-label="commitment-section" className="px-10">
+        <div className="h-[400px] flex items-center justify-center">
+          <div className="flex justify-center flex-col items-center text-center space-y-3 max-w-[800px]">
+            <span className="text-orange-400 font-bold text-xl">
+              KOMITMEN TEUAS
+            </span>
+            <h1 className="text-6xl font-bold tracking-wide leading-[72px]">
+              Fokus Utama Kami
+            </h1>
+            <p className="text-gray-400">
+              IKA TEUAS hadir untuk membangun jaringanalumni yang solid,
+              mendukung prestasi mahasiswa, membuka peluang karir, dan
+              memberikan akses pembelajaran bagi lulusan Tekik Elektro UPI.
+              Jelajahi fitur utama kami dan temukan manfaatnya untuk masa
+              depannmu!
+            </p>
+          </div>
+        </div>
+      </section>
+
+      <section aria-label="testimony-section" className="px-10">
+        <div className="h-[800px] grid grid-cols-2 place-items-center">
+          <div>
+            <div className="bg-gray-500 h-[500px] w-[500px] rounded-2xl flex items-center justify-center">
+              <ImageIcon className="h-32 w-32 text-white" />
+            </div>
+          </div>
+          <div className="space-y-5 max-w-[750px]">
+            <span className="text-orange-400 font-bold text-xl">
+              TESTIMONI ALUMNI
+            </span>
+            <h1 className="text-6xl font-bold tracking-wide leading-[72px]">
+              Cerita Sukses Alumni TEUAS
+            </h1>
+            <p className="text-gray-400">
+              Jaringan alumni Teknik Elektro UPI telah membantu banyak lulusan
+              dalam membangun karir, memperluas koneksi, dan mendapatkan wawasan
+              industri. Simak kisah inspiratif para alumni yang telah meraih
+              kesuksesan di berbagai bidang!
+            </p>
+            <Button size="lg" className="bg-orange-600">
+              Gabung Sekarang
+            </Button>
+          </div>
+        </div>
+      </section>
+
+      <section
+        aria-label="news-section"
+        className="px-10 bg-emerald-900 rounded-t-4xl"
+      >
+        <div className="h-[900px] py-10 space-y-10">
+          <div className="flex justify-between">
+            <h1 className="text-6xl font-bold tracking-wide leading-[72px] text-white">
+              Berita & Kegiatan
+            </h1>
+            <Button className="bg-orange-600 " size="lg">
+              Lebih Lanjut
+            </Button>
+          </div>
+          <div className="overflow-x-scroll py-4">
+            <div className="flex gap-10 w-fit">
+              {[1, 2, 3, 4, 5].map((item) => (
+                <div
+                  key={item}
+                  className="bg-gray-400 h-[600px] w-[500px] rounded-2xl flex items-center justify-center"
+                >
+                  <ImageIcon className="h-32 w-32 text-white" />
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <footer className="px-10 bg-neutral-800">
+        <div className="h-[400px] py-10 text-white">
+          <div className="grid grid-cols-3">
+            <div className="bg-gray-400 h-[200px] w-[200px] rounded-2xl flex items-center justify-center">
+              <ImageIcon className="h-16 w-16" />
+            </div>
+            <div>
+              <h3>Navigation</h3>
+              <p>nav</p>
+              <p>nav</p>
+              <p>nav</p>
+            </div>
+            <div>
+              <h3>Address</h3>
+              <p>nav</p>
+              <p>nav</p>
+              <p>nav</p>
+            </div>
+          </div>
+        </div>
       </footer>
     </div>
   );
